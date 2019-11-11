5357d3a309f26f005ecdf74a242fa6f35dbea276
Abimael <abimael.rza@gmail.com>
2019-11-10 23:38:54 -0300

    iniciada estilização da página Cadastro do Egresso

--- frontEnd/src/pages/CadastroEgresso/index.jsx
+++ frontEnd/src/pages/CadastroEgresso/index.jsx
@@ -1,11 +1,71 @@
 import React from 'react';
+import '../../styles/main.css';
+import egressoIcon from '../../imgs/egressoIcon.png';
+
+
+
 
 function CadastroEgresso() {
     return (
         <div>
-            <div className="container clearfix">
+            <div className="page_body">
+                <div className='H1_box'>
+                    <img src={egressoIcon} height='50' width='50' alt='logo_egresso' />
+                    <h1 className='H1_text'>Cadastro de Egressos</h1>
+                </div>
 
-                <h1>Esse componente é a página de cadastro de Egressos</h1>
+                <div className='box_row'>
+                    <div className='box_column'>
+                        <div className="form-group">
+                            <label className='simple_text'>Nome</label>
+                            <input type="text" className="form-control form-control-lg" name="nomeEgresso" id="nomeEgresso" required />
+                        </div>
+
+                        <div className="form-group">
+                            <label className='simple_text'>Curso</label>
+                            <input type="text" className="form-control form-control-lg" name="cursoEgresso" id="cursoEgresso" required />
+                        </div>
+
+                        <div className="form-group">
+                            <label className='simple_text'>Instituição</label>
+                            <input type="text" className="form-control form-control-lg" name="instituicaoEgresso" id="instituicaoEgresso" required />
+                        </div>
+
+                        <div className="form-group">
+                            <label className='simple_text'>Data de Conclusão</label>
+                            <input type="text" className="form-control form-control-lg" name="dataConclusao" id="dataConclusao" required />
+                        </div>
+                    </div>
+
+                    <div className='box_column'>
+                        <div className="form-group">
+                            <label className='simple_text'>Atuando na Area</label>
+                            <input type="text" className="form-control form-control-lg" name="nomeEgresso" id="nomeEgresso" required />
+                        </div>
+
+                        <div className="form-group">
+                            <label className='simple_text'>Telefone</label>
+                            <input type="text" className="form-control form-control-lg" name="cursoEgresso" id="cursoEgresso" required />
+                        </div>
+
+                        <div className="form-group">
+                            <label className='simple_text'>Email</label>
+                            <input type="text" className="form-control form-control-lg" name="instituicaoEgresso" id="instituicaoEgresso" required />
+                        </div>
+
+                        <div className="form-group">
+                            <label className='simple_text'>Data de Conclusão</label>
+                            <input type="text" className="form-control form-control-lg" name="dataConclusao" id="dataConclusao" required />
+                        </div>
+
+                        <div className="form-group">
+                            <button type="button" class="btn btn-primary">Cadastrar</button>
+                        </div>
+                    </div>
+
+
+                </div>
+
 
 
             </div>
