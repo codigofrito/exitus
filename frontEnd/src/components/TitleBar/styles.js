38e6bf08b95f880c039a4a5b6ccb2a53bdcebf8c
Marcelo Machado <marcelo.cruz.machado@hotmail.com>
2019-12-09 10:25:16 -0300

    Fixando imagem das ondas no bottom da div pai

--- frontEnd/src/components/TitleBar/styles.js
+++ frontEnd/src/components/TitleBar/styles.js
@@ -3,8 +3,8 @@ import fundo from "../../assets/fundo.svg";
 
 export const Ondas = styled.img.attrs(props => ({
 }))`
-    position: relative;
-    margin-top: -90px; 
+    position: absolute;
+    bottom: 0px;
     width: 100%
 `;
 
@@ -13,19 +13,15 @@ export const BgImageBox = styled.div`
     height: 220px;
     position: relative;
     background: ${props => props.theme.primaryColor};   
-    background-image: url("https://startbootstrap.com/assets/img/overlay.svg"),linear-gradient(45deg,${props => props.theme.primaryColor} 0%,#006fe6 100%);
     background-size: cover;
     z-index: 0;
-    padding: 1rem 0 4rem;
 
 `;
 
 export const Titulo = styled.div`
-
     color: #fff;
     font-size: 30px;
     font-weight: bold;
-
 `;
 
 export const Descricao = styled.div`
